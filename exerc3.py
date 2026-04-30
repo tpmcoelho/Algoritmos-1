ea3bd967ee34cac7342123bf7192fa617ce7ad6a
tpmcoelho <tpmcoelho@gmail.com>
2026-04-30 18:19:20 -0300

    Delete exerc3.py

--- exerc3.py
+++
@@ -1,7 +0,0 @@
-x = 0
-y = 0
-def quociente(x, y):
-    x = int(input("Digite o primeiro número: "))
-    y = int(input("Digite o segundo número: "))
-    print (f"O quociente de {x} e {y} é: {x / y}")
-quociente(x, y)
