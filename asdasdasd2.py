a7f4ed33731724d82556f714b73e843966774af2
tpmcoelho <tpmcoelho@gmail.com>
2026-04-30 18:18:20 -0300

    Delete asdasdasd2.py

--- asdasdasd2.py
+++
@@ -1,8 +0,0 @@
-nome = ""
-n = 0
-def imprime_n_vezes(nome, n):
-    nome = input("Digite algo: ")
-    n = int(input("Digite um número: "))
-    for i in range(n):
-        print(f"{nome}")
-imprime_n_vezes(nome, n)
