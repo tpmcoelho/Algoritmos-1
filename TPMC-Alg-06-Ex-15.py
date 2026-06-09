8e34dcde3892387af305d4eec6184c8ad9907c2e
tpmcoelho <tpmcoelho@gmail.com>
2026-06-09 14:23:41 -0300

    Add files via upload

--- TPMC-Alg-06-Ex-15.py
+++ TPMC-Alg-06-Ex-15.py
@@ -1,19 +1,47 @@
-def tokens (x):
+def tokenizar(expressao):
     tokens = []
+    numero = ""
     i = 0
-    while i in len(x):
-        c = x[i]
+
+    while i < len(expressao):
+        c = expressao[i]
+
         if c == " ":
-            i = i + 1
+            i += 1
             continue
+
+        elif c in "*/()":
+            if numero != "":
+                tokens.append(numero)
+                numero = ""
+            tokens.append(c)
+
         elif c in "+-":
-            tokens.append(c)
-        elif c.isdigit:
-            tokens.append(c)
+            j = i - 1
+
+            while j >= 0 and expressao[j] == " ":
+                j -= 1
+
+            if j >= 0 and (expressao[j].isdigit() or expressao[j] == ')'):
+                if numero != "":
+                    tokens.append(numero)
+                    numero = ""
+                tokens.append(c)
+            else:
+                numero += c
+
+        else:
+            numero += c
+
+        i += 1
+
+    if numero != "":
+        tokens.append(numero)
+
     return tokens
-    
+
 def main():
-    x = input("Digite a sua expressão matemática")
-    print (f"Os tokens baseados na sua ecpressão matemática são: {tokens(x)}")
-    
-main ()
+    expressao = input("Digite uma expressão matemática: ")
+    print(tokenizar(expressao))
+
+main()
